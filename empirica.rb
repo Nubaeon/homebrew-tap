bcb5c71323ebd9f0fa416da15494b3e8f35d5738
Empirica System <david@getempirica.com>
2026-04-29 15:40:13 +0200

    Update empirica to 1.8.16

--- empirica.rb
+++ empirica.rb
@@ -13,8 +13,8 @@ class Empirica < Formula
 
   desc "Epistemic self-assessment framework for AI agents"
   homepage "https://github.com/Nubaeon/empirica"
-  url "https://files.pythonhosted.org/packages/source/e/empirica/empirica-1.8.15.tar.gz"
-  sha256 "c1710b3a65a5a06039162f4f9842b3115331b46085470d9f96b12e38e287c4af"
+  url "https://files.pythonhosted.org/packages/source/e/empirica/empirica-1.8.16.tar.gz"
+  sha256 "bf6d575f7bb5a95b9cc674dee57f631dca9619f2ee182a107e2ab26439eb44a7"
   license "MIT"
 
   depends_on "python@3.11"
@@ -49,7 +49,7 @@ class Empirica < Formula
 
   test do
     # Test that the CLI works
-    assert_match "1.8.15", shell_output("#{bin}/empirica --version")
+    assert_match "1.8.16", shell_output("#{bin}/empirica --version")
 
     # Test that key commands exist
     system "#{bin}/empirica", "session-create", "--help"
